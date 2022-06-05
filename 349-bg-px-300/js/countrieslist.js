4516d01c3f302425ae2d92833547749cbe631cba
Pavel Bashkeev <pavelbashkeev@gmail.com>
2022-06-05 12:12:35 +0300

    update

---
+++ 349-bg-px-300/js/countrieslist.js
@@ -0,0 +1,1 @@
+!function(){"use strict";window.countryList={bg:{name:"България",code:"bg",price:59,oldPrice:118,labelPrice:"лева",phoneHelper:"Например, +359 123456789",nameHelper:"Jivko Djurovic",nameError:"Името е задължително",phoneError:"Телефонният номер е задължителен",countryError:"Страната е задължителна",isDefault:!0}}}();
